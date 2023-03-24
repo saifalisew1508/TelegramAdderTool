fe5b2cfce63a804c4cbdb157a1706975eda966a3
Saif Ali <sachinsaif03@gmail.com>
2023-03-24 16:03:03 +0530

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -4,8 +4,43 @@ from pyrogram.client import Client, enums
 from helper.filter import filterus
 from helper.data import get_data
 from pathlib import Path
+from colorama import init, Fore
+import pyfiglet
+import os, random
+
+lg = Fore.LIGHTGREEN_EX
+rs = Fore.RESET
+r = Fore.RED
+w = Fore.WHITE
+cy = Fore.CYAN
+
+
+info = lg + '(' + w + 'i' + lg + ')' + rs
+error = lg + '(' + r + '!' + lg + ')' + rs
+success = w + '(' + lg + '+' + w + ')' + rs
+INPUT = lg + '(' + cy + '~' + lg + ')' + rs
+colors = [lg, w, r, cy]
+
+
+def banner():
+    f = pyfiglet.Figlet(font='slant')
+    logo = f.renderText('Tele Adder')
+    print(random.choice(colors) + logo + rs)
+    
+def clr():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+clr()
+banner()
+print(f'  {r}Version: {w}3.1 {r}| Author: {w}SAIF ALI{rs}\n')
+print(f'  {r}Telegram {w}@PrinceXofficial {r}| Instagram: {w}@saifalisew1508{rs}\n')
+
+
 #workdir = 'session/'
-method = input('choose method username or id: ').lower()
+method = input('Choose Method Username or ID: ').lower()
 async def main():
      root = Path.cwd()
      config = (json.load(open(root / "config.json")))
