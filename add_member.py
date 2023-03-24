bca22989bdddd0d0203b1bb46e5aa44753a61ddb
Saif Ali <sachinsaif03@gmail.com>
2023-03-24 16:01:09 +0530

    Update add_member.py

--- add_member.py
+++ add_member.py
@@ -1,8 +1,42 @@
-
 import asyncio
 import json
 from itertools import dropwhile
 from helper.account_handler import add_member, updatecount, addlogin
+from colorama import init, Fore
+import pyfiglet
+import os, random
+
+lg = Fore.LIGHTGREEN_EX
+rs = Fore.RESET
+r = Fore.RED
+w = Fore.WHITE
+cy = Fore.CYAN
+
+
+info = lg + '(' + w + 'i' + lg + ')' + rs
+error = lg + '(' + r + '!' + lg + ')' + rs
+success = w + '(' + lg + '+' + w + ')' + rs
+INPUT = lg + '(' + cy + '~' + lg + ')' + rs
+colors = [lg, w, r, cy]
+
+
+def banner():
+    f = pyfiglet.Figlet(font='slant')
+    logo = f.renderText('Tele Adder')
+    print(random.choice(colors) + logo + rs)
+    
+def clr():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+clr()
+banner()
+print(f'  {r}Version: {w}3.1 {r}| Author: {w}SAIF ALI{rs}\n')
+print(f'  {r}Telegram {w}@PrinceXofficial {r}| Instagram: {w}@saifalisew1508{rs}\n')
+
+
 #option for choose username or id
 option = input('choose method username or id: ').lower() 
 async def main():
